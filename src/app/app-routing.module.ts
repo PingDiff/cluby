2e124ce943f3833c37a260e70e5d9a6bc31f6f66
PingDiff <yass821998@outlook.fr>
2021-12-21 15:08:03 +0100

    fixing repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { RechercheComponent } from './recherche/recherche.component';
 
 
 const routes: Routes = [
+  { path: '#', component: TimelineComponent },
+  { path: '', component: TimelineComponent },
   { path: 'feed', component: TimelineComponent },
   { path: 'search', component: RechercheComponent },
 ];
